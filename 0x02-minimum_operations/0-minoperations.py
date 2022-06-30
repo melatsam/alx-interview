c0e179d97cb30e70b0ac58bff7acec842c0496eb
unknown <88444516+melatsam@users.noreply.github.com>
2022-06-30 11:42:10 -0700

    integer

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+calculate the fewest number of operations needed to
+result in exactly n H charater in the file.
+"""
+
+def minOperation(n):
+    """ 
+    returns an interger
+    """
+
+    n = 4
+    print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+    n = 12
+    print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+    return 0
+    
