14f6404a4ddd0573c4404ec990703daaedcdf9d5
unknown <88444516+melatsam@users.noreply.github.com>
2022-08-07 08:56:49 -0700

    2d_matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Test oxo7 - Rotate 2D Matrix
+"""
+def rotate_2d_matrix(matrix: [[int]]) -> [[int]]:
+    size = len(matrix)
+    layer_count = int(size / 2)
+
+    for layer in range(0, layer_count):
+        last = size - layer - 1
+
+        for element in range(layer, last):
+            offset = element - layer
+
+            top = matrix[layer][element]
+            right_side = matrix[element][last]
+            bottom = matrix[last][last - offset]
+            left_side = matrix[last - offset][layer]
+
+            matrix[layer][element] = left_side
+            matrix[element][last] = top
+            matrix[last][last - offset] = right_side
+            matrix[last - offset][layer] = bottom
+    return matrix
