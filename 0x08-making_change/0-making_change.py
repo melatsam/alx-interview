1f4b51dbae189a164c1d41e371e79558991a2cab
unknown <88444516+melatsam@users.noreply.github.com>
2022-08-14 15:10:33 -0700

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,13 +2,25 @@
 '''Main file for testing'''
 
 
-def makeChange(coins, total: List[int], amount: int) -> int:
-    dp = []
-    for i in range(amount + 1):
-        dp.append(float('inf'))
-    dp[0] = 0
-    for amt in range(1, amount + 1):
-        for coin in coins:
-            if coin <= amt:
-                dp[amt] = min(dp[amt], dp[amt-coin]+1)
-    return dp[amount] if dp[amount] != float('inf') else -1
+def makeChange(coins, total):
+    matrix = [[0 for m in range(change_amount + 1)] for m in range(len(coin_set) + 1)]
+    for i in range(change_amount + 1):
+        matrix[0][i] = i
+    return matrix
+
+# Function we'll use to optimize the default above matrix
+def makeChange(coins, total):
+    matrix = _change_matrix(coins, change)
+    for c in range(1, len(coins) + 1):
+        for r in range(1, change + 1):
+
+            if coins[c-1] == r:
+                matrix[c][r] = 1
+
+            elif coins[c-1] > r:
+                matrix[c][r] = matrix[c-1][r]
+
+            else:
+                matrix[c][r] = min(matrix[c - 1][r], 1 + matrix[c][r - coins[c - 1]])
+
+    return matrix[-1][-1]
