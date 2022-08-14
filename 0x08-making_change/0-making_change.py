65cbf91205f64c842043fdf95036bed86b0eae5a
unknown <88444516+melatsam@users.noreply.github.com>
2022-08-14 15:18:45 -0700

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,24 +3,19 @@
 
 
 def makeChange(coins, total):
-    matrix = [[0 for m in range(change_amount + 1)] for m in range(len(coin_set) + 1)]
-    for i in range(change_amount + 1):
-        matrix[0][i] = i
-    return matrix
 
-# Function we'll use to optimize the default above matrix
-def makeChange(coins, total):
-    matrix = _change_matrix(coins, change)
-    for c in range(1, len(coins) + 1):
-        for r in range(1, change + 1):
+    dp = [0] * (sum + 1)
 
-            if coins[c-1] == r:
-                matrix[c][r] = 1
+    for i in range(1, sum + 1):
 
-            elif coins[c-1] > r:
-                matrix[c][r] = matrix[c-1][r]
+        dp[i] = sys.maxsize
 
-            else:
-                matrix[c][r] = min(matrix[c - 1][r], 1 + matrix[c][r - coins[c - 1]])
+        for c in range(len(S)):
+            if i - S[c] >= 0:
+                result = dp[i - S[c]]
 
-    return matrix[-1][-1]
+                if result != sys.maxsize:
+                    dp[i] = min(dp[i], result + 1)
+
+    return dp[sum]
+
