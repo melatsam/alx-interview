0de8acca44fcaebafbe8876337e6fb06bfc9d8dc
unknown <88444516+melatsam@users.noreply.github.com>
2022-08-14 18:42:18 -0700

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,13 +2,26 @@
 '''Main file for testing'''
 
 
-def makeChange(coins, total: List[int], total: int) -> int:
-    dp = []
-    for i in range(total + 1):
-        dp.append(float('inf'))
-    dp[0] = 0
-    for coin in range(1, total + 1):
-        for coin in coins:
-            if coin <= 0:
-                dp[a] = min(dp[a], dp[a-coin]+1)
-    return dp[total] if dp[total] != float('inf') else -1
+def makeChange(coins: List[int], total: int, solutions: List[int]) -> int:
+    if total < 0:
+        return -1
+    if total == 0:
+        return 0
+
+    if coin[total - 1] != 0:
+        return solutions[total - 1]
+
+    optimal_solution = float('inf')
+
+    for coin in coins:
+        best_solution_for_coin = coin_change_sub(coins, total - coin, solutions)
+        if 0 <= best_solution_for_coin < optimal_solution:
+            optimal_solution = best_solution_for_coin + 1
+
+    if optimal_solution == float('inf'):
+        solutions[total - 1] = -1
+    else:
+        solutions[total - 1] = optimal_solution
+
+    return solutions[total - 1]
+    return -1
