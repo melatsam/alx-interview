baa14b9a29feec02210f1c64b4eee3568f80c4a4
unknown <88444516+melatsam@users.noreply.github.com>
2022-08-21 12:00:40 -0700

    island

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -10,8 +10,10 @@ def island_perimeter(grid):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 perimeter += 4
+                # Each cell is square, with a side lengeth of 1
                 if i > 0 and grid[i-1][j] == 1:
                     perimeter -= 2
+                # cells are connected horizontally/vertically(not diagonally)
                 if j > 0 and grid[i][j-1] == 1:
                     perimeter -= 2
     return perimeter
