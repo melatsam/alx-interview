6e57856a965c308119f151169cbe8c302e439954
unknown <88444516+melatsam@users.noreply.github.com>
2022-08-25 12:25:10 -0700

    boxs

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" 
+boxs is a list of lists
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    keys with the same numbers
+    """
+    for key in range(1, len(boxes) - 1):
+        res = False
+        for index in range(len(boxes)):
+            res = key in boxes[index] and key != index
+            if res:
+                break
+        if res is False:
+            return res
+    return True
