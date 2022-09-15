e607ee1de49dfddfa01debf0172bc956cfe9f073
unknown <88444516+melatsam@users.noreply.github.com>
2022-09-14 23:00:19 -0700

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-''' fewest number of the coin '''
+"""fewest number of coins needed to meet"""
 
 
 def makeChange(coins, total):
-    dp = [total + 1] * (total + 1)
-    dp[0] = 0
+    """the value of coin will always be an integer greater than 0
+    you can assume you have an infinite number of each denomination of coin"""
+    if total <= 0:
+        return 0
 
-    for a in range(1, total + 1):
-        for coin in coins:
-            if a - coin >= 0:
-                dp[a] = min(dp[a], 1 + dp[a - coin])
-
-    if dp[total] != (total + 1):
-        return dp[total]
+    current_total = 0
+    coin_used = 0
+    coins = sorted(coins, reverse=True)
+    for coin in coins:
+        r = (total-current_total)//coin
+        current_total += r*coin
+        coin_used += r
+        if current_total == total:
+            return coin_used
     return -1
